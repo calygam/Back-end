706a23ea31894b61c63ec80d5a8eb83be8de345e
CaioBO <caiobuenooliveira1210@gmail.com>
2025-10-02 19:49:05 -0300

    Update WebConfigurer.java

--- src/main/java/com/calygam/back/configs/WebConfigurer.java
+++ src/main/java/com/calygam/back/configs/WebConfigurer.java
@@ -15,7 +15,8 @@ public class WebConfigurer implements WebMvcConfigurer {
             .allowedOrigins(
                 "https://happy-wave-0b04a110f.6.azurestaticapps.net",
                 "https://blue-desert-0c09d7910.1.azurestaticapps.net",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://10.0.0.191:8081"
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
